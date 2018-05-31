bb28e46a3e9f85a24e71af7d0282ce9c5786dd9e
NCLAN\ewe <ewe@netcompany.com>
2018-05-31 23:04:38 +0200

    Adding new medicine to database

--- app/src/main/java/trochimiuk/kaniewski/czaplicka/kwod/pl/healthcareapp/Medicine/NewMedicineActivity.java
+++ app/src/main/java/trochimiuk/kaniewski/czaplicka/kwod/pl/healthcareapp/Medicine/NewMedicineActivity.java
@@ -1,20 +1,33 @@
 package trochimiuk.kaniewski.czaplicka.kwod.pl.healthcareapp.Medicine;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import trochimiuk.kaniewski.czaplicka.kwod.pl.healthcareapp.DatabaseHelper;
 import trochimiuk.kaniewski.czaplicka.kwod.pl.healthcareapp.R;
 
 public class NewMedicineActivity extends AppCompatActivity {
+
+    private DatabaseHelper healthCareDb;
+    private Spinner medicineSpinner;
+    private List<String> names = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_medicine);
         Button addMedicineBtn = (Button) findViewById(R.id.addNewMedBtn);
+
         addMedicineBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,5 +35,28 @@ public class NewMedicineActivity extends AppCompatActivity {
                 startActivity(medicineIntent);
             }
         });
+
+        medicineSpinner = (Spinner) findViewById(R.id.medicineSpinner);
+        healthCareDb = new DatabaseHelper(this);
+        ArrayList<Medicine> medicinesList = new ArrayList<>();
+        Cursor data = healthCareDb.getListContents();
+        if(data.getCount() == 0){
+            Toast.makeText(this, "There are no contents in this list!",Toast.LENGTH_LONG).show();
+        }else{
+            while(data.moveToNext()){
+                names.add(data.getString(1));
+                medicinesList.add(new Medicine(data.getString(1),data.getString(2)));
+                ArrayAdapter<String> medicineAdapter = new ArrayAdapter<String>(NewMedicineActivity.this,android.R.layout.simple_spinner_item,names);
+                medicineAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                medicineSpinner.setAdapter(medicineAdapter);
+            }
+
+        }
+        /*Intent intent = getIntent();
+        Toast.makeText(this,intent.getStringExtra("dbSuccess"),  Toast.LENGTH_LONG).show();
+        /*if(!(intent.getStringExtra("dbSuccess").isEmpty())){
+            Toast.makeText(getApplicationContext(),intent.getStringExtra("dbSuccess"),  Toast.LENGTH_LONG).show();
+        }*/
+
     }
 }
--- app/src/main/java/trochimiuk/kaniewski/czaplicka/kwod/pl/healthcareapp/Medicine/NewMedicineInDBActivity.java
+++ app/src/main/java/trochimiuk/kaniewski/czaplicka/kwod/pl/healthcareapp/Medicine/NewMedicineInDBActivity.java
@@ -1,15 +1,56 @@
 package trochimiuk.kaniewski.czaplicka.kwod.pl.healthcareapp.Medicine;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import trochimiuk.kaniewski.czaplicka.kwod.pl.healthcareapp.DatabaseHelper;
 import trochimiuk.kaniewski.czaplicka.kwod.pl.healthcareapp.R;
 
 public class NewMedicineInDBActivity extends AppCompatActivity {
+
+    private DatabaseHelper healthCareDb;
+    private Button saveNewMed;
+    private EditText newMedName, newDescName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_medicine_in_db);
+        newMedName = (EditText) findViewById(R.id.newMedName);
+        newDescName = (EditText) findViewById(R.id.newDescName);
+        saveNewMed = (Button) findViewById(R.id.saveNewMed);
+
+        healthCareDb = new DatabaseHelper(this);
+
+        saveNewMed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(newMedName.length()!= 0 && newDescName.length()!= 0 ){
+                    AddData(new Medicine(newMedName.getText().toString(),newDescName.getText().toString()));
+                    newMedName.setText("");
+                    newDescName.setText("");
+                }else{
+                    Toast.makeText(NewMedicineInDBActivity.this, "Wprowadź wszystkie dane!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
+
+    public void AddData(Medicine medicine) {
+        boolean insertData = healthCareDb.addData(medicine);
+        if(insertData==true){
+            Intent medicineIntent = new Intent(getApplicationContext(),NewMedicineActivity.class);
+            medicineIntent.putExtra("dbSuccess", "Lek dodany do bazy danych!");
+            startActivity(medicineIntent);
+        }else{
+            Toast.makeText(this, "Wystapił błąd", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
 }
